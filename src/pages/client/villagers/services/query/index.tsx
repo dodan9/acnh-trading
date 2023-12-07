8f96b5f09f95dd52c8ac1ede8c8c93193dbf3cf5
도단구 <9139a90@naver.com>
2023-12-07 13:58:11 +0900

    gh-page setting

--- src/pages/client/villagers/services/query/index.tsx
+++ src/pages/client/villagers/services/query/index.tsx
@@ -2,12 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import { getVillagerListApi } from "../api";
 import { query_key } from "@src/services/query/query_key";
 import { useVillagerFilter } from "../../store/filter";
+import { VillagerFilterType } from "../../types";
 
-export const useVillagerListQuery = () => {
+export const useVillagerListQuery = (query?: VillagerFilterType) => {
   const filter = useVillagerFilter();
 
   const getVillagerList = async () => {
-    const response = await getVillagerListApi(filter);
+    const response = await getVillagerListApi({ ...filter, ...query });
     return response.data;
   };
 
